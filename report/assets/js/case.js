54cbcbc69574fa1dee11d5f8d534ad72a6204bab
Yoshiya Watanabe <Yoshiya.Watanabe@ccc.co.jp>
2018-05-09 11:30:18 +0900

    [FIX] report template

--- report/assets/js/case.js
+++ report/assets/js/case.js
@@ -1,8 +1,28 @@
 $(function(){
   console.log('++ case ++');
+  // Init - [ Highlight ]
   $('pre code').each(
     function(idx, block){
       hljs.highlightBlock(block);
+    }
+  );
+  // Init - [ Digest Toggle ]
+  var digestTriggerStack = {};
+  var digestTriggers = $('.j_digest_detail_trigger');
+  var digestToggleAreas = $('.j_toggle');
+  digestToggleAreas.each(
+    function(idx, digest){
+      digestTriggerStack[$(digest).attr('data-target-url')] = $(digest);
+    }
+  );
+  digestTriggers.each(
+    function(idx, trigger){
+      $(trigger).on(
+        'click',
+        function(){
+          digestTriggerStack[$(this).attr('data-target-url')].slideToggle()
+        }
+      )
     }
   );
 });
