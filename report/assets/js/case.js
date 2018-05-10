41b6c8208fc9a4326f7c158233704851128fe0b2
Yoshiya Watanabe <Yoshiya.Watanabe@ccc.co.jp>
2018-05-10 15:55:16 +0900

    [FIX] #12
    ・Async Load Limit

--- report/assets/js/case.js
+++ report/assets/js/case.js
@@ -25,17 +25,57 @@ $(function(){
     }
   );
   // Init - Async Load
-  $('.j_async_image_load').each(
-    function(idx, imgWrapper){
-      var tmpImg = $(document.createElement('img'));
-      $(imgWrapper).append(tmpImg);
-      tmpImg.on(
-        'load',
-        function(){
-          tmpImg.addClass('___loaded');
-        }
-      );
-      tmpImg.attr('src', $(imgWrapper).attr('data-async-src'));
+  var asyncImages = $('.j_async_image_load').toArray();
+  var asyncImagesNum = asyncImages.length;
+  console.warn('Total Image [ ' + asyncImagesNum + ' ]');
+  var ASYNC_LOOP_START_INDEX = 0;
+  var ASYNC_LOOP_STOP_INDEX = ASYNC_LOOP_RATE = 30;
+  var DEFS = [];
+  var asyncLoader = function(wrapperDOM){
+    var dfd = new $.Deferred();
+    var tmpImg = $(document.createElement('img'));
+    wrapperDOM.append(tmpImg);
+    tmpImg.on(
+      'load',
+      function(){
+        var loadedImg = $(this);
+        loadedImg.addClass('___loaded');
+        dfd.resolve();
+      }
+    );
+    tmpImg.on(
+      'error',
+      function(){
+        var loadedImg = $(this);
+        loadedImg
+          .addClass('___error')
+          .attr(
+            'src',
+            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACAAQMAAAD58POIAAAABlBMVEUAAAAAFx8t7DCsAAAAAXRSTlMAQObYZgAAACdJREFUSMdj+A8EDEjkqMCoADI5ClDAoImXUYHBKTAKRvPLqADRAgAGUnu9KI2EPgAAAABJRU5ErkJggg=='
+          )
+        dfd.resolve();
+      }
+    )
+    tmpImg.attr('src', wrapperDOM.attr('data-async-src'));
+    return dfd.promise();
+  }
+  var asyncLoad = function(){
+    DEFS = []
+    var asyncLoadImages = asyncImages.slice(ASYNC_LOOP_START_INDEX, ASYNC_LOOP_STOP_INDEX);
+    for(var i = 0, il = asyncLoadImages.length; i < il; i++){
+      DEFS.push(asyncLoader($(asyncLoadImages[i])));
     }
-  )
+    $.when.apply($, DEFS).done(
+      function(){
+        ASYNC_LOOP_START_INDEX = ASYNC_LOOP_START_INDEX + ASYNC_LOOP_RATE;
+        ASYNC_LOOP_STOP_INDEX = ASYNC_LOOP_START_INDEX + ASYNC_LOOP_RATE
+        if(ASYNC_LOOP_START_INDEX >= asyncImagesNum){
+          console.log('All Images Downloaded');
+        }else{
+          asyncLoad();
+        }
+      }
+    );
+  }
+  asyncLoad();
 });
--- reset.sh
+++ reset.sh
@@ -11,3 +11,15 @@ rm -rf ___cache/*
 
 # Clear Screenshot
 rm -rf ___screenshot/*
+
+# Clear TMP
+rm -rf ___tmp/*
+
+# Clear Download Images
+rm -rf report/assets/image/**
+
+# Clear Style Result
+rm -rf report/assets/json/**
+
+# Clear Case Report
+rm -rf report/case/___*.html
