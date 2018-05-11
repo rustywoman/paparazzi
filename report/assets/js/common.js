7cd7c719b71ce8c6058e9c57bce29c1d26d92c6a
Yoshiya Watanabe <Yoshiya.Watanabe@ccc.co.jp>
2018-05-11 13:43:19 +0900

    [REF]

--- report/assets/js/common.js
+++ report/assets/js/common.js
@@ -1,3 +1,105 @@
-$(function(){
-  console.log('++ common ++');
-});
+(function(){
+  // -----------------------------------
+  // Marker Handler Class
+  // -----------------------------------
+  class MarkerHandler{
+    constructor(){
+      this.markerDOM = null;
+      this.overlayDOM = [];
+    }
+    update(elm, status){
+      var dfd = new $.Deferred();
+      setTimeout(
+        function(){
+          elm.addClass('___visible');
+          dfd.resolve();
+        },
+        200 * status
+      );
+      return dfd.promise();
+    };
+    reset(){
+      this.markerDOM = $('.marker');
+      var tmpOverlayDOM = null;
+      var _this = this;
+      this.markerDOM.each(
+        function(idx, marker){
+          tmpOverlayDOM = $('<span class="overlay"></span>');
+          _this.overlayDOM.push(tmpOverlayDOM);
+          $(marker).append(tmpOverlayDOM);
+        }
+      );
+    };
+    init(){
+      var dfd = new $.Deferred();
+      var defs = [];
+      for(var i = 1, il = this.overlayDOM.length; i <= il; i++){
+        defs.push(this.update($(this.overlayDOM[i - 1]), i));
+      }
+      $.when.apply($, defs).done(
+        function(){
+          dfd.resolve();
+        }
+      );
+      return dfd.promise();
+    }
+  };
+  // -----------------------------------
+  // Loading Handler Class
+  // -----------------------------------
+  class LoadingHandler{
+    constructor(loadingDOM, loadingStatusDOM){
+      this.loadingDOM = loadingDOM;
+      this.loadingStatusDOM = loadingStatusDOM;
+      this.currentLoadingStatus = 0;
+    };
+    update(status){
+      var dfd = new $.Deferred();
+      var buffer = status * 30;
+      var visibleStatus = status + '%';
+      var _this = this;
+      setTimeout(
+        function(){
+          _this.loadingStatusDOM.text(visibleStatus).attr('data-text', visibleStatus);
+          dfd.resolve();
+        },
+        buffer
+      );
+      return dfd.promise();
+    };
+    init(status){
+      var dfd = new $.Deferred();
+      var defs = [];
+      var _this = this;
+      if(this.currentLoadingStatus !== 0){
+        for(var i = this.currentLoadingStatus; i <= status; i++){
+          defs.push(this.update(i));
+        }
+      }else{
+        for(var i = 1; i <= status; i++){
+          defs.push(this.update(i));
+        }
+      }
+      $.when.apply($, defs).done(
+        function(){
+          _this.currentLoadingStatus = status;
+          if(_this.currentLoadingStatus === 100){
+            setTimeout(
+              function(){
+                _this.loadingDOM.addClass('___loaded');
+                dfd.resolve();
+              },
+              800
+            );
+          }else{
+            dfd.resolve();
+          }
+        }
+      );
+      return dfd.promise();
+    };
+  };
+  // Export
+  window.LoadingHandler = LoadingHandler;
+  window.MarkerHandler = MarkerHandler;
+})();
--- report/assets/js/top.js
+++ report/assets/js/top.js
@@ -1,50 +1,8 @@
 $(function(){
   var reportLinkRoot = $('#l_content__report_table');
-  var loading = $('#loading__bg');
-  var loadingStatus = $('#loading__status');
+  var loadingHandlerIns = new LoadingHandler($('#loading__bg'), $('#loading__status'));
   var reportResultEmptyDOM = document.createDocumentFragment();
-  var currentLoadingStatus = 0;
-  var updateLoadingStatus = function(status){
-    var dfd = new $.Deferred();
-    var buffer = status * 40;
-    setTimeout(
-      function(){
-        loadingStatus.text(status + '%').attr('data-text', status + '%');
-        dfd.resolve();
-      },
-      buffer
-    );
-    return dfd.promise();
-  };
-  var loadingHandler = function(status){
-    var dfd = new $.Deferred();
-    var defs = [];
-    if(currentLoadingStatus !== 0){
-      for(var i = currentLoadingStatus; i <= status; i++){
-        defs.push(updateLoadingStatus(i));
-      }
-    }else{
-      for(var i = 1; i <= status; i++){
-        defs.push(updateLoadingStatus(i));
-      }
-    }
-    $.when.apply($, defs).done(
-      function(){
-        currentLoadingStatus = status;
-        if(currentLoadingStatus === 100){
-          setTimeout(
-            function(){
-              dfd.resolve();
-            },
-            1.2
-          );
-        }else{
-          dfd.resolve();
-        }
-      }
-    );
-    return dfd.promise();
-  };
+  var markerHandlerIns = new MarkerHandler();
   // Init
   $.ajax(
     {
@@ -54,38 +12,51 @@ $(function(){
     }
   ).then(
     function(result){
-      loadingHandler(60).then(
-        function(){
-          var tmpResultInfo = null;
-          for(var i = 0, il = result.length; i < il; i++){
-            tmpResultInfo = result[i];
-            reportResultEmptyDOM.appendChild(
-              $(
-                [
-                  '<div class="m_content__report_table__row">',
-                    '<div class="m_content__report_table__cell">',
-                      '<a class="marker" href="' + tmpResultInfo['path'] +  '" target="_blank">',
-                        '<span>' + tmpResultInfo['name'] + '</span>',
-                      '</a>',
-                    '</div>',
-                    '<div class="m_content__report_table__cell">' + tmpResultInfo['date'] + '</div>',
-                  '</div>'
-                ].join('')
-              ).get(0)
-            );
-          }
-          reportLinkRoot.get(0).appendChild(reportResultEmptyDOM);
-          loadingHandler(80).then(
-            function(){
-              loadingHandler(100).then(
-                function(){
-                  loading.addClass('___loaded');
-                }
+      loadingHandlerIns
+        .init(60)
+        .then(
+          function(){
+            var tmpResultInfo = null;
+            for(var i = 0, il = result.length; i < il; i++){
+              tmpResultInfo = result[i];
+              reportResultEmptyDOM.appendChild(
+                $(
+                  [
+                    '<div class="m_content__report_table__row">',
+                      '<div class="m_content__report_table__cell">',
+                        '<a class="marker" href="' + tmpResultInfo['path'] +  '" target="_blank">',
+                          '<span>' + tmpResultInfo['name'] + '</span>',
+                        '</a>',
+                      '</div>',
+                      '<div class="m_content__report_table__cell">' + tmpResultInfo['date'] + '</div>',
+                    '</div>'
+                  ].join('')
+                ).get(0)
               );
             }
-          );
-        }
-      );
+            reportLinkRoot.get(0).appendChild(reportResultEmptyDOM);
+            loadingHandlerIns
+              .init(80)
+              .then(
+                function(){
+                  markerHandlerIns.reset();
+                  loadingHandlerIns
+                    .init(100)
+                    .then(
+                      function(){
+                        markerHandlerIns
+                          .init()
+                          .then(
+                            function(){
+                              console.info('ALL DONE');
+                            }
+                          );
+                      }
+                    );
+                }
+              );
+          }
+        );
     },
     function(){
       // ToDo
