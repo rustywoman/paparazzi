800bd6f90092bbf89fd932b9293842ff5b6cc16d
Yoshiya Watanabe <Yoshiya.Watanabe@ccc.co.jp>
2018-05-21 12:15:59 +0900

    [FIX] #15

--- python_modules/doms.py
+++ python_modules/doms.py
@@ -14,6 +14,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # Import
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 import constant
+import textwrap
 
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -26,13 +27,11 @@ def generateTextsInfo(textsInfo):
     '''
     textInfoDOM = ''
     for textInfo in textsInfo:
-        textInfoDOM = textInfoDOM + constant.BR.join(
-            [
-                '<div class="m_content__css_digest_table__row">',
-                '<div class="m_content__css_digest_table__cell">{0}</div>'.format(textInfo),
-                '</div>'
-            ]
-        )
+        textInfoDOM = textInfoDOM + textwrap.dedent('''
+            <div class="m_content__css_digest_table__row">
+              <div class="m_content__css_digest_table__cell">{0}</div>
+            </div>
+        ''').format(textInfo).strip()
     return textInfoDOM
 
 
@@ -50,16 +49,17 @@ def generateImagesInfo(testName, imagesInfo):
             imageDownLoadStatus = 'x'
         else:
             imageDownLoadStatus = '-'
-        imageInfoDOM = imageInfoDOM + constant.BR.join(
-            [
-                '<div class="m_content__image_digest_table__row">',
-                '<div class="m_content__image_digest_table__cell">{0}</div>'.format(imageInfo['path']['raw']),
-                '<div class="m_content__image_digest_table__cell">{0}</div>'.format(imageDownLoadStatus),
-                '<div class="m_content__image_digest_table__cell j_async_image_load" data-async-src="/{0}/{1}/{2}/{3}">'.format('assets', 'image', testName, imageInfo['path']['local']),
-                '</div>',
-                '</div>'
-            ]
-        )
+        imageInfoDOM = imageInfoDOM + textwrap.dedent('''
+            <div class="m_content__image_digest_table__row">
+              <div class="m_content__image_digest_table__cell">{path}</div>
+              <div class="m_content__image_digest_table__cell">{status}</div>
+              <div class="m_content__image_digest_table__cell j_async_image_load" data-async-src="{asyncSrc}"></div>
+            </div>
+        ''').format(
+            path=imageInfo['path']['raw'],
+            status=imageDownLoadStatus,
+            asyncSrc='/assets/image/' + testName + '/' + imageInfo['path']['local']
+        ).strip()
     return imageInfoDOM
 
 
@@ -68,16 +68,22 @@ def generateCssGeneralInfo(cssGeneralInfo):
      @param  cssGeneralInfo - Css Information
      @return String DOM for Css Information - [ general ]
     '''
-    return constant.BR.join(
-        [
-            '<div class="m_content__css_digest_table__row">',
-            '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssGeneralInfo['total']),
-            '<div class="m_content__css_digest_table__cell">{0} ( {1} % )</div>'.format(cssGeneralInfo['ok']['count'], cssGeneralInfo['ok']['rate']),
-            '<div class="m_content__css_digest_table__cell">{0} ( {1} % )</div>'.format(cssGeneralInfo['ng']['count'], cssGeneralInfo['ng']['rate']),
-            '<div class="m_content__css_digest_table__cell">{0} ( {1} % )</div>'.format(cssGeneralInfo['unknown']['count'], cssGeneralInfo['unknown']['rate']),
-            '</div>'
-        ]
-    )
+    return textwrap.dedent('''
+        <div class="m_content__css_digest_table__row">
+          <div class="m_content__css_digest_table__cell">{total}</div>
+          <div class="m_content__css_digest_table__cell">{okCount} ( {okRate} % )</div>
+          <div class="m_content__css_digest_table__cell">{ngCount} ( {ngRate} % )</div>
+          <div class="m_content__css_digest_table__cell">{unknownCount} ( {unknownRate} % )</div>
+        </div>
+    ''').format(
+        total=cssGeneralInfo['total'],
+        okCount=cssGeneralInfo['ok']['count'],
+        okRate=cssGeneralInfo['ok']['rate'],
+        ngCount=cssGeneralInfo['ng']['count'],
+        ngRate=cssGeneralInfo['ng']['rate'],
+        unknownCount=cssGeneralInfo['unknown']['count'],
+        unknownRate=cssGeneralInfo['unknown']['rate']
+    ).strip()
 
 
 def generateCssDetailInfo(parseResult):
@@ -91,90 +97,102 @@ def generateCssDetailInfo(parseResult):
         if cssInfo['path'] == 'inline':
             cssSelectorInfoDOM = ''
             for cssSelectorInfo in cssInfo['detail']:
-                cssSelectorInfoDOM = cssSelectorInfoDOM + constant.BR.join(
-                    [
-                        '<div class="m_content__css_table__row">',
-                        '<div class="m_content__css_table__cell">{0}</div>'.format(cssSelectorInfo['selector']),
-                        '<div class="m_content__css_table__cell">{0}</div>'.format(cssSelectorInfo['count']),
-                        '</div>'
-                    ]
-                )
-            inlineCssInfoDOM = inlineCssInfoDOM + constant.BR.join(
-                [
-                    '<li class="m_section__ref_css_list__item">',
-                    '<p class="m_section__css_digest_detail_trigger_wrapper">',
-                    '<a class="j_digest_detail_trigger marker" href="javascript:void(0);" data-target-url="inline_css">More</a>',
-                    '</p>',
-                    '<div class="j_toggle" data-target-url="inline_css">',
-                    '<div class="m_section__css_digest_table">',
-                    '<div class="m_content__css_digest_table__row">',
-                    '<div class="m_content__css_digest_table__cell ___head">Total</div>',
-                    '<div class="m_content__css_digest_table__cell ___head">OK</div>',
-                    '<div class="m_content__css_digest_table__cell ___head">NG</div>',
-                    '<div class="m_content__css_digest_table__cell ___head">Unknown</div>',
-                    '</div>',
-                    '<div class="m_content__css_digest_table__row">',
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['total']),
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['ok']),
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['ng']),
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['unknown']),
-                    '</div>',
-                    '</div>'
-                    '<div class="m_content__css_table">',
-                    '<div class="m_content__css_table__row">',
-                    '<div class="m_content__css_table__cell ___head">Selector Definition</div>',
-                    '<div class="m_content__css_table__cell ___head">Matched DOM Count</div>',
-                    '</div>',
-                    cssSelectorInfoDOM,
-                    '</div>',
-                    '</div>',
-                    '</li>'
-                ]
-            )
+                cssSelectorInfoDOM = cssSelectorInfoDOM + textwrap.dedent('''
+                    <div class="m_content__css_table__row">
+                      <div class="m_content__css_table__cell">{selector}</div>
+                      <div class="m_content__css_table__cell">{count}</div>
+                    </div>
+                ''').format(
+                    selector=cssSelectorInfo['selector'],
+                    count=cssSelectorInfo['count']
+                ).strip()
+            inlineCssInfoDOM = inlineCssInfoDOM + textwrap.dedent('''
+                <li class="m_section__ref_css_list__item">
+                  <p class="m_section__css_digest_detail_trigger_wrapper">
+                    <a class="j_digest_detail_trigger marker" href="javascript:void(0);" data-target-url="inline_css">More</a>
+                  </p>
+                  <div class="j_toggle" data-target-url="inline_css">
+                    <div class="m_section__css_digest_table">
+                      <div class="m_content__css_digest_table__row">
+                        <div class="m_content__css_digest_table__cell ___head">Total</div>
+                        <div class="m_content__css_digest_table__cell ___head">OK</div>
+                        <div class="m_content__css_digest_table__cell ___head">NG</div>
+                        <div class="m_content__css_digest_table__cell ___head">Unknown</div>
+                      </div>
+                      <div class="m_content__css_digest_table__row">
+                        <div class="m_content__css_digest_table__cell">{total}</div>
+                        <div class="m_content__css_digest_table__cell">{okCount}</div>
+                        <div class="m_content__css_digest_table__cell">{ngCount}</div>
+                        <div class="m_content__css_digest_table__cell">{unknownCount}</div>
+                      </div>
+                    </div>
+                    <div class="m_content__css_table">
+                      <div class="m_content__css_table__row">
+                        <div class="m_content__css_table__cell ___head">Selector Definition</div>
+                        <div class="m_content__css_table__cell ___head">Matched DOM Count</div>
+                      </div>
+                      {cssSelectorInfoDOM}
+                    </div>
+                  </div>
+                </li>
+            ''').format(
+                total=cssInfo['digest']['total'],
+                okCount=cssInfo['digest']['ok'],
+                ngCount=cssInfo['digest']['ng'],
+                unknownCount=cssInfo['digest']['unknown'],
+                cssSelectorInfoDOM=cssSelectorInfoDOM
+            ).strip()
         else:
             cssSelectorInfoDOM = ''
             for cssSelectorInfo in cssInfo['detail']:
-                cssSelectorInfoDOM = cssSelectorInfoDOM + constant.BR.join(
-                    [
-                        '<div class="m_content__css_table__row">',
-                        '<div class="m_content__css_table__cell">{0}</div>'.format(cssSelectorInfo['selector']),
-                        '<div class="m_content__css_table__cell">{0}</div>'.format(cssSelectorInfo['count']),
-                        '</div>'
-                    ]
-                )
-            refCssInfoDOM = refCssInfoDOM + constant.BR.join(
-                [
-                    '<li class="m_section__ref_css_list__item">',
-                    '<h4 class="m_section__ref_css__file_name">{0}</h4>'.format(cssInfo['path']),
-                    '<p class="m_section__css_digest_detail_trigger_wrapper">',
-                    '<a class="j_digest_detail_trigger marker" href="javascript:void(0);" data-target-url="{0}">More</a>'.format(cssInfo['path']),
-                    '</p>',
-                    '<div class="j_toggle" data-target-url="{0}">'.format(cssInfo['path']),
-                    '<div class="m_section__css_digest_table">',
-                    '<div class="m_content__css_digest_table__row">',
-                    '<div class="m_content__css_digest_table__cell ___head">Total</div>',
-                    '<div class="m_content__css_digest_table__cell ___head">OK</div>',
-                    '<div class="m_content__css_digest_table__cell ___head">NG</div>',
-                    '<div class="m_content__css_digest_table__cell ___head">Unknown</div>',
-                    '</div>',
-                    '<div class="m_content__css_digest_table__row">',
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['total']),
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['ok']),
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['ng']),
-                    '<div class="m_content__css_digest_table__cell">{0}</div>'.format(cssInfo['digest']['unknown']),
-                    '</div>',
-                    '</div>'
-                    '<div class="m_content__css_table">',
-                    '<div class="m_content__css_table__row">',
-                    '<div class="m_content__css_table__cell ___head">Selector Definition</div>',
-                    '<div class="m_content__css_table__cell ___head">Matched DOM Count</div>',
-                    '</div>',
-                    cssSelectorInfoDOM,
-                    '</div>',
-                    '</div>',
-                    '</li>'
-                ]
-            )
+                cssSelectorInfoDOM = cssSelectorInfoDOM + textwrap.dedent('''
+                    <div class="m_content__css_table__row">
+                      <div class="m_content__css_table__cell">{selector}</div>
+                      <div class="m_content__css_table__cell">{count}</div>
+                    </div>
+                ''').format(
+                    selector=cssSelectorInfo['selector'],
+                    count=cssSelectorInfo['count']
+                ).strip()
+
+            refCssInfoDOM = refCssInfoDOM + textwrap.dedent('''
+                <li class="m_section__ref_css_list__item">
+                  <h4 class="m_section__ref_css__file_name">{path}</h4>
+                  <p class="m_section__css_digest_detail_trigger_wrapper">
+                    <a class="j_digest_detail_trigger marker" href="javascript:void(0);" data-target-url="{path}">More</a>
+                  </p>
+                  <div class="j_toggle" data-target-url="{path}">
+                    <div class="m_section__css_digest_table">
+                      <div class="m_content__css_digest_table__row">
+                        <div class="m_content__css_digest_table__cell ___head">Total</div>
+                        <div class="m_content__css_digest_table__cell ___head">OK</div>
+                        <div class="m_content__css_digest_table__cell ___head">NG</div>
+                        <div class="m_content__css_digest_table__cell ___head">Unknown</div>
+                      </div>
+                      <div class="m_content__css_digest_table__row">
+                        <div class="m_content__css_digest_table__cell">{total}</div>
+                        <div class="m_content__css_digest_table__cell">{okCount}</div>
+                        <div class="m_content__css_digest_table__cell">{ngCount}</div>
+                        <div class="m_content__css_digest_table__cell">{unknownCount}</div>
+                      </div>
+                    </div>
+                    <div class="m_content__css_table">
+                      <div class="m_content__css_table__row">
+                        <div class="m_content__css_table__cell ___head">Selector Definition</div>
+                        <div class="m_content__css_table__cell ___head">Matched DOM Count</div>
+                      </div>
+                      {cssSelectorInfoDOM}
+                    </div>
+                  </div>
+                </li>
+            ''').format(
+                path=cssInfo['path'],
+                total=cssInfo['digest']['total'],
+                okCount=cssInfo['digest']['ok'],
+                ngCount=cssInfo['digest']['ng'],
+                unknownCount=cssInfo['digest']['unknown'],
+                cssSelectorInfoDOM=cssSelectorInfoDOM
+            ).strip()
     return {
         'refCssInfoDOM': refCssInfoDOM,
         'inlineCssInfoDOM': inlineCssInfoDOM
@@ -188,14 +206,14 @@ def generateRefScriptInfo(refScriptsInfo):
     '''
     refScriptInfoDOM = ''
     for refScriptInfo in refScriptsInfo:
-        refScriptInfoDOM = refScriptInfoDOM + constant.BR.join(
-            [
-                '<li class="m_section__ref_js_list__item">',
-                '<h4 class="m_section__ref_js__file_name">{0}</h4>'.format(refScriptInfo),
-                '<p class="m_section__css_digest_detail_trigger_wrapper">',
-                '<a class="marker" href="{0}" target="_blank">More</a>'.format(refScriptInfo),
-                '</p>',
-                '</li>'
-            ]
-        )
+        refScriptInfoDOM = refScriptInfoDOM + textwrap.dedent('''
+            <li class="m_section__ref_js_list__item">
+              <h4 class="m_section__ref_js__file_name">{refScript}</h4>
+              <p class="m_section__css_digest_detail_trigger_wrapper">
+                <a class="marker" href="{refScript}" target="_blank">More</a>
+              </p>
+            </li>
+        ''').format(
+            refScript=refScriptInfo
+        ).strip()
     return refScriptInfoDOM
