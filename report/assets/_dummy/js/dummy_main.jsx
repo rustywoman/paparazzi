e5615ea2e97a873563c55ba7107df4ab85e7948c
Yoshiya Watanabe <Yoshiya.Watanabe@ccc.co.jp>
2018-06-05 12:06:17 +0900

    [UPDATE] dummy

--- report/assets/_dummy/js/dummy_main.jsx
+++ report/assets/_dummy/js/dummy_main.jsx
@@ -5,22 +5,14 @@
       <h2>local test page</h2>
       <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, delectus culpa dolor libero voluptatum incidunt quae! Quasi consequatur, quod. Sint, quisquam magni ullam culpa sapiente maxime eaque consequatur atque tempora.</h3>
       <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, delectus culpa dolor libero voluptatum incidunt quae! Quasi consequatur, quod. Sint, quisquam magni ullam culpa sapiente maxime eaque consequatur atque tempora.</h4>
-      <div>
-        <p>
-          <span>DIVDIV</span>
-        </p>
-      </div>
-      <p>
-        <i>III</i>
-      </p>
       <p>
         <img src="/assets/_dummy/image/1.png" alt="1_png" />
       </p>
       <p>
         <img src="/assets/_dummy/image/1.gif" alt="1_gif" />
       </p>
-      <a href="/sub/A/">SUB_A</a>
-      <a href="https://www.yahoo.co.jp/">[ outer ] YAHOO</a>
+      <a href="/sub/A/">[ inner link ] - SUB_A</a>
+      <a href="https://www.yahoo.co.jp/">[ outer link ] - YAHOO</a>
       <div id="buffer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
       <p>End</p>
     </div>,
