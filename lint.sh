4618b7150be7083de4c76bf5b098932943e188d5
rustywoman <rustywoman@gmail.com>
2018-06-27 14:58:50 +0900

    [ADD] sass-lint

--- lint.sh
+++ lint.sh
@@ -10,6 +10,26 @@ tslint -c tslint.json front_end/ts/klass/MarkerHandler.ts
 tslint -c tslint.json front_end/ts/klass/LoadingHandler.ts
 tslint -c tslint.json front_end/ts/klass/DependencyTreeHandler.ts
 
+# For SCSS
+sass-lint 'front_end/scss/common/_base.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/common/_keyframes.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/common/_mixin.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/common/_reset.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/common/_settings.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/_top.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/_detail.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/_error.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_button.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_footer.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_header.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_loading.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_marker.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_overlay.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_tree.scss' -v -q --config .sass-lint.yml
+sass-lint 'front_end/scss/page/part/_zoom.scss' -v -q --config .sass-lint.yml
+# sass-lint 'front_end/scss/_highlight.scss' -v -q --config .sass-lint.yml
+# sass-lint 'front_end/scss/_perfect_scroll.scss' -v -q --config .sass-lint.yml
+
 # For Python
 conf=conf/paparazzi.conf
 pycodestyle python_modules/*.py --config=$conf --verbose --show-source --format '%(code)s: %(text)s' --benchmark
